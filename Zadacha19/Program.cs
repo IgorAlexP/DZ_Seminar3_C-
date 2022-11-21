70082379ab94782a62938737da6743f7ca42c06a
Igor Popkov <i.popkov@CPSURAL.LOCAL>
2022-11-21 14:23:40 +0500

    решение задачи 19

--- Zadacha19/Program.cs
+++ Zadacha19/Program.cs
@@ -3,3 +3,16 @@
 14212 -> нет
 12821 -> да
 23432 -> да*/
+Console.Write("ВВедите любое пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+String str = Convert.ToString(number);
+char [] ar = str.ToCharArray();
+
+if (ar[0] == ar[4] && ar[1] == ar[3] && ar[2] == ar[2])
+{
+    Console.WriteLine(str + " - ЭТО ЧИСЛО ЯВЛЯЕТСЯ ПАЛИНДРОМОМ");
+}
+else
+{
+    Console.WriteLine(str + " - ЭТО ЧИСЛО НЕ ЯВЛЯЕТСЯ ПАЛИНДРОМОМ");
+}
